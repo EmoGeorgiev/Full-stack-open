a7d2905cb0dd04e57d9020840506d2fcf054c480
EmoGeorgiev <unstabl3@Emo>
2024-03-22 01:27:37 +0200

    Added solution to exercises 2.4 and 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,12 +2,12 @@ import Header from "./Header"
 import Content from "./Content"
 import Total from "./Total"
 
-const Course = ({ course }) => {
+const Course = ({ name, parts }) => {
   return (
     <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={course.parts.map(part => part.exercises)} />
+      <Header name={name} />
+      <Content parts={parts} />
+      <Total exercises={parts.map(part => part.exercises)} />
     </>
   )
 }
