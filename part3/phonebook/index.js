ad3b0f5d5e4afcf97e5a20b0949b968bdd206b58
EmoGeorgiev <unstabl3@Emo>
2024-03-25 17:28:06 +0200

    Added solution to exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -43,6 +43,13 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    phonebook = phonebook.filter(person => person.id !== id)
+    console.log(phonebook)
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
