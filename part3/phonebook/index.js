50e22d73ec0bd1b39b3c902cbdf6a8f34f8819c7
EmoGeorgiev <unstabl3@Emo>
2024-03-26 16:44:43 +0200

    Added solution to exercise 3.9 and 3.10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 let phonebook = [
     { 
@@ -26,6 +27,7 @@ let phonebook = [
 ]
 
 app.use(express.json())
+app.use(cors())
 
 app.use(morgan((tokens, req, res) => {
     return [
@@ -81,7 +83,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
