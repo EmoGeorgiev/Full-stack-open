fd5503552a26481fe26ecd23991b387056157dce
EmoGeorgiev <unstabl3@Emo>
2024-05-28 18:14:45 +0300

    Added solution to exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const [details, setDetails] = useState(false)
+
+  const hideDetails = { display: details ? 'none' : '' }
+  const showDetails = { display: details ? '' : 'none' }
+
+  const toggleDetails = () => {
+    setDetails(!details)
+  }
+
+  return(
+    <div style={blogStyle}>
+      <div style={hideDetails}>
+        {blog.title} {blog.author} <button onClick={toggleDetails}>view</button>
+      </div>
+      <div style={showDetails}>
+        <p>{blog.title} <button onClick={toggleDetails}>hide</button></p>
+        <p>{blog.url}</p>
+        <p>{blog.likes} <button>like</button></p>
+        <p>{blog.author}</p>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
